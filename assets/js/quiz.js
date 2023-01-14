bc26dfce0e12e0fd027431fec131809de6823ee9
unknown <allenpip341@gmail.com>
2023-01-14 11:22:15 +0000

    started the timer function and set up an empty objects array for questions

---
+++ assets/js/quiz.js
@@ -0,0 +1,51 @@
+let timer = 5;
+//this function will run for the whole game. use clearInterval calling the last question as a parameter
+let start = setInterval(function(){
+    timer--
+console.log(timer)
+},1000)
+clearInterval(5)
+//timer is now player score
+
+let qusestions = [
+    {
+
+    },
+
+    {
+
+    },
+
+    {
+
+    },
+
+    {
+
+    },
+
+    {
+
+    },
+
+    {
+
+    },
+
+    {
+
+    },
+
+    {
+
+    },
+
+    {
+
+    },
+
+    {
+
+    },
+
+]
